b8edc0ff1b72d8769d945d96e8aee10e3fb10ff1
carlobisda <38801646+carlobisda@users.noreply.github.com>
2021-03-10 07:00:48 -0500

    Create tkinter.py

---
+++ gui-template/tkinter.py
@@ -0,0 +1,9 @@
+from tkinter import * #this will import everything from tkinter
+
+window=Tk() #case sensitive, will create empty Win10 window
+
+
+
+
+window.mainloop() 
+#always in the end, will ensure window is always open
